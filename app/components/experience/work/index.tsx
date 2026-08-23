1bd1ed7b81ac50c2f67ed596cfd01a07573c78ac
Deeptanil <deeptanilsinha27@gmail.com>
2026-08-23 13:22:22 +0530

    feat: add Projects and Work 3D components with responsive background textures and icons

--- app/components/experience/work/index.tsx
+++ app/components/experience/work/index.tsx
@@ -1,14 +1,38 @@
 'use client';
 
-import { Image } from '@react-three/drei';
+import { Image, useTexture } from '@react-three/drei';
+import { isMobile } from 'react-device-detect';
+import { useMemo } from 'react';
 
 const Work = () => {
+  const paintingTexture = useTexture(isMobile ? '/painting_m.webp' : '/painting.webp');
+
+  const bgTexture = useMemo(() => {
+    const tex = paintingTexture.clone();
+    tex.needsUpdate = true;
+    if (isMobile) {
+      tex.repeat.set(1.0, 0.5);
+      tex.offset.set(0.0, 0.5); // Top half for Work tile on mobile
+    } else {
+      tex.repeat.set(0.5, 1.0);
+      tex.offset.set(0.0, 0.0); // Left half for Work tile on PC
+    }
+    return tex;
+  }, [paintingTexture]);
+
   return (
     <group position={[0, 0, 0]}>
+      {/* Spanning Background Painting */}
+      <mesh position={[0, 0, 0]}>
+        <planeGeometry args={[4, 4]} />
+        <meshBasicMaterial map={bgTexture} transparent opacity={0.88} />
+      </mesh>
+
+      {/* Foreground Icon */}
       <Image
         url="/Enchanted_Book.gif"
         transparent
-        scale={[2.5, 2.5]}
+        scale={[2.2, 2.2]}
         position={[0, 0, 0.1]}
       />
     </group>
